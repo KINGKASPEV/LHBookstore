7d778b72623da244946e82faf3dfaaef260ecbac
Kingkaspev <kingsleychiboy22@gmail.com>
2024-01-05 18:49:58 +0100

    Service Configurations

--- src/LHBookstore.Application/LHBookstore.Application/ServiceExtensions/DIExtension.cs
+++ src/LHBookstore.Application/LHBookstore.Application/ServiceExtensions/DIExtension.cs
@@ -1,6 +1,25 @@
-﻿namespace LHBookstore.Application.ServiceExtensions
+﻿using LHBookstore.Application.Implementations;
+using LHBookstore.Application.Implementations.Books;
+using LHBookstore.Application.Implementations.Orders;
+using LHBookstore.Application.Services;
+using LHBookstore.Application.Services.Books;
+using LHBookstore.Application.Services.Orders;
+using LHBookstore.Infrastructure;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.EntityFrameworkCore;
+
+namespace LHBookstore.Application.ServiceExtensions
 {
-    public class DIExtension
+    public static class DIExtension
     {
+        public static void AddDependencies(this IServiceCollection services, IConfiguration config)
+        {
+            //services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IBookServices, BookServices>();
+            services.AddScoped<IOrderService, OrderService>();
+            
+            services.AddDbContext<LHBContext>(options =>
+            options.UseSqlite(config.GetConnectionString("DefaultConnection")));
+        }
     }
 }
